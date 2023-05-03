52c0e208dece542a580c9d99862a0448a0ff296e
fernandopaparelli <rm551022@fiap.com.br>
2023-05-02 22:47:35 -0300

    Foreach

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
-alert("Alo Mundo!");
+// var nome = "Luizinho";
+
+// if(nome != ""){
+//     let nome = "Nicolly";
+// }
+
+// console.log(nome);
+//Quando declaramos uma variável com var
+// o Javascript aplica um processo nestas que chamamos de HOISTING.
+//O HOISTING ou elevação, coloca todas as variáveis do tipo var no topo do programa.
+//Isso faz com que se existirem variáveis com mesmo nome, elas se sobrepõem.
+// let nr1 = 10;
+// let nr2 = 5;
+// //IF TERNÁRIO - DESCOBRINDO SE O NÚMERO É PAR OU IMPAR
+// let resultado = !(nr1 % nr2) ? "PAR" : "IMPAR";
+// console.log(resultado);
+
+//Recuperar um elemento do HTML com a função getElementById(parâmetro)
+// const mn = document.getElementById("menu");
+// console.log(mn.textContent);
+
+//Recuperando uma collection de elementos...
+const aElements = document.getElementsByTagName("a");
+
+
+//Declarando um array em Javascript
+let nr1 = [1,2,3,4,5];
+let nr2 = [6,7,8,9,10];
+// let nr3 = [nr1,nr2];
+console.log(nr1);
+console.log(nr2);
+
+//OPERADOR SPREAD ...
+let nr3 = [...nr1,...nr2];
+console.log("NOVO ARRAY " + nr3);
+
+nr3.forEach( (numero)=>{
+    console.log("ITEM DO ARRAY : " + numero);
+} );
+
+const imgElements = [...document.getElementsByClassName("img")];
+imgElements.forEach((img)=>{
+    console.log(`Path das imagens : ${img.src}`);
+});
